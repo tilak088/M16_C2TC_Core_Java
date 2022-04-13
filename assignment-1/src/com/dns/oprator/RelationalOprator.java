c3707df3e3a582e6f3f5f2ab8587b87a0f216e46
tilak088 <tilakchandele431@gmail.com>
2022-04-13 02:20:16 -0600

    first commit

---
+++ assignment-1/src/com/dns/oprator/RelationalOprator.java
@@ -0,0 +1,17 @@
+package com.dns.oprator;
+
+public class RelationalOprator {
+
+	public static void main(String[] args)
+	{
+		Scanner = new Scanner(System.in)
+				int a=9.nextInt();
+		        int b=5.nextInt();
+		 boolean int res=a>b
+				System.out.println(res);
+		 s.close();
+		
+
+	}
+
+}
