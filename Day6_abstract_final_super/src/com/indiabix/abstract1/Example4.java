ef21cc666bbcc8e7a3a52824126348e20e1d7df1
tilak088 <tilakchandele431@gmail.com>
2022-04-14 09:11:03 -0600

    first commit

---
+++ Day6_abstract_final_super/src/com/indiabix/abstract1/Example4.java
@@ -0,0 +1,10 @@
+package com.indiabix.abstract1;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day6_abstract_final_super/src/com/indiabix/abstract1/package-info.java
@@ -0,0 +1,1 @@
+package com.indiabix.abstract1;
