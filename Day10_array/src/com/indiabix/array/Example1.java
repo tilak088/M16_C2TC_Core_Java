0a73fdab838699e7f6c81ac9f571e7b68f876a12
tilak088 <tilakchandele431@gmail.com>
2022-04-21 00:55:25 -0600

    first commit

---
+++ Day10_array/src/com/indiabix/array/Example1.java
@@ -0,0 +1,12 @@
+package com.indiabix.array;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int arr[]= {55,66,77,88,99};
+		System.out.println(arr[0]);
+		System.out.println(arr[5]);
+
+	}
+
+}
