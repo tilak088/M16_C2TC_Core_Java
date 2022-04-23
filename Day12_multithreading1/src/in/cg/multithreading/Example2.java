e0096f09680ae5a488816ff62de1a0a0939f57f0
tilak088 <tilakchandele431@gmail.com>
2022-04-23 02:25:36 -0600

    first commit

---
+++ Day12_multithreading1/src/in/cg/multithreading/Example2.java
@@ -0,0 +1,20 @@
+package in.cg.multithreading;
+
+class A extends Thread
+{
+	public void run()
+	{
+		System.out.println("Thread is running");
+	}
+}
+public class Example2  {
+
+	public static void main(String[] args)
+	{
+		A obj=new A();
+		obj.start();
+		obj.start();
+
+	}
+
+}
---
+++ Day12_multithreading1/src/in/cg/multithreading/Example3.java
@@ -0,0 +1,22 @@
+package in.cg.multithreading;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		Thread t=Thread.currentThread();
+		System.out.println("Current thread: "+t);
+		t.setName("Priya");
+		t.setPriority(9);
+		System.out.println("Current thread: "+t);
+		try
+		{
+			Thread.sleep(1);
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println(e);
+		}
+
+	}
+
+}
