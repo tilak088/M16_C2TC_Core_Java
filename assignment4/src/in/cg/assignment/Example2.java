32802380a9960de12371e6f13b0f7f37d8f22bb4
tilak088 <tilakchandele431@gmail.com>
2022-05-05 11:23:25 -0600

    first commit

---
+++ assignment4/src/in/cg/assignment/Example2.java
@@ -0,0 +1,23 @@
+package in.cg.assignment;
+
+public class Example2 {
+
+	static class Circle {
+		
+	    static int josephus(int n)
+	    {
+	        int p = 1;
+	        while (p <= n)
+	            p *= 2;
+	 
+	        return (2 * n) - p + 1;
+	    }
+	 
+	    public static void main(String[] args)
+	    {
+	        int n = 5;
+	 
+	        System.out.println("The chosen place is "+ josephus(n));
+	    }
+	}
+}
