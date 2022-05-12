0de2f336923cbe62e5892a6b442317dcd2d892db
tilak088 <tilakchandele431@gmail.com>
2022-05-12 02:14:46 -0600

    first commit

---
+++ Day21_Stream API/src/com/indiabix/streams/Example1.java
@@ -0,0 +1,17 @@
+package com.indiabix.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		List<Integer>obj=Arrays.asList(10,20,30,12,17,6);
+		obj.stream().filter(i->i>15).forEach(i->System.out.println(i));
+
+
+		
+
+	}
+
+}
---
+++ Day21_Stream API/src/com/indiabix/streams/Example4.java
@@ -0,0 +1,16 @@
+package com.indiabix.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		List<Integer>obj=Arrays.asList(10,20,6,30,12,17,6);
+		List<Integer>obj2=obj.stream().map(i->i*i).collect(Collectors.toList());
+		System.out.println(obj2);
+
+	}
+
+}
