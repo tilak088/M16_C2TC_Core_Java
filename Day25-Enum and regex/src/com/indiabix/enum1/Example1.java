b988f08650b6324a2af828fd4ed005bec6a7e122
tilak088 <tilakchandele431@gmail.com>
2022-05-27 04:21:45 -0600

    first commit

---
+++ Day25-Enum and regex/src/com/indiabix/enum1/Example1.java
@@ -0,0 +1,15 @@
+package com.indiabix.enum1;
+enum Car
+{
+	Mercedes,BMW,Audi;
+}
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Car c=Car.Mercedes;
+		System.out.println(c);
+
+	}
+
+}
---
+++ Day25-Enum and regex/src/com/indiabix/enum1/Example2.java
@@ -0,0 +1,19 @@
+package com.indiabix.enum1;
+
+public class Example2 
+{
+	enum Margin
+	{
+		TOP,BOTTOM,LEFT,RIGHT;
+	}
+
+	public static void main(String[] args) {
+		int count=11;
+		for(Margin m:Margin.values())
+		{
+			System.out.printf("Margin:%d =%s\n",count++,m);
+		}
+
+	}
+
+}
