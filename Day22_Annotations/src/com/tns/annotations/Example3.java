8fe5d5ff20169f5af39c0cee297934c4e6655e85
tilak088 <tilakchandele431@gmail.com>
2022-05-27 04:18:38 -0600

    first commit

---
+++ Day22_Annotations/src/com/tns/annotations/Example3.java
@@ -0,0 +1,22 @@
+package com.tns.annotations;
+class Animal
+{
+	@Deprecated
+	void sound()
+	{
+		System.out.println("Roar");
+	}
+	void print()
+	{
+		System.out.println("Animal sound");
+	}
+}
+public class Example3 {
+
+	public static void main(String[] args) {
+		Animal a=new Animal();
+		a.print();
+
+	}
+
+}
