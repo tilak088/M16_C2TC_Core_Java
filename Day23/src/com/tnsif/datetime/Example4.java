6f351165769e32d01551ea35bc7cb1efb82c4071
tilak088 <tilakchandele431@gmail.com>
2022-05-27 04:19:19 -0600

    first commit

---
+++ Day23/src/com/tnsif/datetime/Example4.java
@@ -0,0 +1,14 @@
+package com.tnsif.datetime;
+
+import java.time.Clock;
+
+public class Example4 
+{
+	public static void main(String[] args) 
+	{
+		Clock c=Clock.systemUTC();
+		Clock c1=Clock.systemDefaultZone();
+		System.out.println(c1);
+	}
+
+}
